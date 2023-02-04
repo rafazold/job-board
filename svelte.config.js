7fcf22f7285b15b06c8574952a1a959b0f519497
Rafa Zoldan <zoldan.rafael@gmail.com>
2023-02-04 01:33:09 -0500

    :wrench: change to svelte-preprocess to improve sass workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
 
 export default {
-	kit: {
-		adapter: adapter({
-			fallback: '200.html'
-		}),
-	},
-	preprocess: [vitePreprocess()]
+    kit: {
+        adapter: adapter({
+            fallback: '200.html',
+        }),
+    },
+    preprocess: preprocess({
+        scss: {
+            prependData: `@import './src/style/app.scss';`,
+        },
+    }),
 };
