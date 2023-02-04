9f9c492796267191479b481101ce68573de36dc0
Rafa Zoldan <zoldan.rafael@gmail.com>
2023-02-04 01:46:44 -0500

    :recycle: add global designs and shared components / elements design

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<header>
+    <h1 class="title">devjobs</h1>
+    <span />
+</header>
+<main>
+    <slot />
+</main>
+
+<style lang="scss">
+    @import '../style/app';
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,28 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>Welcome to SvelteKit</h2>
+<h3>Welcome to SvelteKit</h3>
+<h4>Welcome to SvelteKit</h4>
+<label class="custom-checkbox">
+    <input
+        class="custom-checkbox"
+        type="checkbox"
+        id="fruit1"
+        name="fruit1"
+        value="Mango"
+        checked
+    />
+    <span>Mango</span>
+</label>
+<button class="button primary-btn">primary</button>
+<button class="button secondary-btn">secondary</button>
+<div class="dark">
+    <button class="button primary-btn">primary</button>
+    <button class="button secondary-btn">secondary</button>
+</div>
 
 <style lang="scss">
-      @import "../style.scss";
+    .dark {
+        padding: 4px 0;
+        background-color: $midnight;
+    }
 </style>
